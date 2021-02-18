e8126e43a658444e3288339f52ca91395430f802
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2021-02-18 23:48:12 +0100

    moved to telegram

--- node-server/src/index.ts
+++ node-server/src/index.ts
@@ -1,5 +1,34 @@
-import { server, port } from './app'
+import { config } from 'dotenv'
+import got from 'got'
+import TelegramBot from 'node-telegram-bot-api'
 
-server.listen(port, () => {
-  console.log(`Wella! node-server is listening to localhost:${port}`)
+import { Message, ProcessQueue } from './queue'
+
+config()
+
+const { MATRIX_SERVER, BOT_TOKEN } = process.env
+
+async function processorFn(msg: Message) {
+  const json = {
+    text: msg.text,
+    color: msg.color,
+  }
+  return got.post(MATRIX_SERVER, { json })
+}
+
+const processQueue = new ProcessQueue(processorFn)
+const bot = new TelegramBot(BOT_TOKEN, { polling: true })
+
+const colors = {
+  'Luca Mattiazzi': 'blue',
+  default: 'green',
+}
+
+bot.on('message', (msg) => {
+  const { id: chatId, first_name, last_name } = msg.chat
+  const { text } = msg
+  const name = `${first_name} ${last_name}`
+  const color = colors[name] || colors.default
+  processQueue.insert({ text, color })
+  bot.sendMessage(chatId, 'Ok grazie, messo in coda!')
 })
